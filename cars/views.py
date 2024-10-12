ca8ceda07390ce7328697a4dd199bc4d941ddaef
Marlon Knupp <marlonjcc23@gmail.com>
2024-10-12 11:42:06 -0300

    ...

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from cars.models import Car
 
-# Create your views here.
+def cars_view(request):
+    cars= Car.objects.all()
+    search= request.GET.get('search')
+
+    if search:
+        cars = cars.filter(model__contains=search)
+    
+    return render(
+        request,
+        'cars.html',
+        {'cars': cars }
+    )
